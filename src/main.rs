9e2ca096c717973756434a5c195f7bbf86c2b907
Ezekiel Victor <codeepoch@gmail.com>
2023-08-28 06:32:47 +0100

    update cors allowed methods

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let cors = Cors::default()
             .allow_any_origin()
+            .allow_any_method()
             .allow_any_header();
         App::new()
             .wrap(cors)
